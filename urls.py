58c50275648015b4310563310eaf1d89eeee4d12
funmi <sannifunmilola26@gmail.com>
2025-11-22 17:02:00 +0100

    docker file adjustment

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+from django.urls import path
+
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
+urlpatterns = [path('health/', health_check)]
